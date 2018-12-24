e7d235d57ea5f882dac907ef42632da8098037ef
Atiladanvi <atila.danvi@outlook.com>
2018-12-24 15:35:39 -0300

    Make blade parts and layont for index

---
+++ src/resources/views/parts/footer.blade.php
@@ -0,0 +1,20 @@
+<footer class="footer">
+   <div class="container-fluid">
+        <nav class="float-left">
+            <ul>
+                <li>
+                    <a href="#">
+                        Home
+                    </a>
+                </li>
+            </ul>
+        </nav>
+        <div class="copyright float-right">
+            &copy;
+            <script>
+                document.write(new Date().getFullYear())
+            </script>, made with <i class="material-icons">favorite</i> by
+            <a href="https://www.creative-tim.com" target="_blank">Creative Tim</a> for a better web.
+        </div>
+   </div>
+</footer>
---
+++ src/resources/views/parts/sidebar.blade.php
@@ -0,0 +1,22 @@
+<div class="sidebar" data-color="purple" data-background-color="white" data-image="{{ asset('vendor/admin/assets/img/sidebar-1.jpg')}}">
+   <!--
+      Tip 1: You can change the color of the sidebar using: data-color="purple | azure | green | orange | danger"
+
+      Tip 2: you can also add an image using data-image tag
+      -->
+   <div class="logo">
+      <a href="#" class="simple-text logo-normal">
+      Material Admin
+      </a>
+   </div>
+   <div class="sidebar-wrapper">
+        <ul class="nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">
+                <i class="material-icons">dashboard</i>
+                <p>Dashboard</p>
+                </a>
+            </li>
+        </ul>
+   </div>
+</div>
