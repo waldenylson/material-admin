1c44ca5438f9e01415171274bd80f03b8876accb
Atiladanvi <atila.danvi@outlook.com>
2018-12-23 21:10:23 -0300

    init

---
+++ src/MaterialAdmin.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace MaterialAdmin;
+
+use Illuminate\Support\ServiceProvider;
+
+class MaterialAdminServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+}
