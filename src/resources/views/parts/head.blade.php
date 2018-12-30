face711374190cb86523dd98119de6c5c6ee71e1
Atiladanvi <atila.danvi@outlook.com>
2018-12-30 00:08:49 -0300

    Removed unnecessary css files

--- src/resources/views/parts/head.blade.php
+++ src/resources/views/parts/head.blade.php
@@ -11,6 +11,6 @@
 <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
 <!-- CSS Files -->
-<link href="{{ asset('vendor/admin/assets/css/material-dashboard.css?v=2.1.1') }}" rel="stylesheet"/>
+<link href="{{ asset('vendor/admin/assets/css/material-dashboard.min.css') }}" rel="stylesheet"/>
 <!-- CSS Just for demo purpose, don't include it in your project -->
 <link href="{{ asset('vendor/admin/assets/demo/demo.css') }}" rel="stylesheet" />
